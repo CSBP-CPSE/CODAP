e1412c4643e2c64cce59bae41a7ff8ce88093a1a
Bruno St-Aubin <staubibr@gmail.com>
2017-06-12 09:35:12 -0400

    update login view for about terms

--- app/src/views/loginView.js
+++ app/src/views/loginView.js
@@ -2,12 +2,16 @@
 r.define(["Api/util/lang",
 		  "Api/util/dom",
 		  "Api/util/animate",
-		  "Exp/components/views/modal"],
+		  "Exp/components/views/modal",
+		  "App/widgets/about",
+		  "App/widgets/terms"],
     
 	function (Lang,
 			  Dom,
 			  Animate,
-			  ModalView) {
+			  ModalView,
+			  AboutWidget,
+			  TermsWidget) {
 
 		var loginView = Lang.Declare("LoginView", [ModalView], { 
 			
@@ -16,6 +20,22 @@ r.define(["Api/util/lang",
 			constructor : function() {
 				Dom.AddCss(this.domNode, "Login");			
 
+				this.popups = {};
+				
+				var opts = {
+					domNode : Dom.Create("div", { className:"About" }, document.body),
+					title 	: Lang.Nls("Settings_About_Note1")
+				}
+				
+				this.popups.About = new AboutWidget(opts);
+				
+				var opts = {
+					domNode : Dom.Create("div", { className:"Terms" }, document.body),
+					title 	: Lang.Nls("Settings_Terms_Note1")
+				}
+				
+				this.popups.Terms = new TermsWidget(opts);
+				
 				this.BuildLogin();
 				
 				this.controller.once("ControllerModelChange", this.onFirstViewNotified_NewModel.bind(this));
@@ -28,8 +48,10 @@ r.define(["Api/util/lang",
 				this.Login.Inner = {};
 				this.Login.Inner.Top = Dom.Create("div", { "className":"Inner" }, this.container);
 				this.Login.Inner.Message = Dom.Create("div", { "className":"Message" }, this.Login.Inner.Top);
-				
-				// this.Login.Inner.Beta = Dom.Create("div", { "className":"Beta" }, this.Login.Inner.Top);
+				this.Login.Inner.About = Dom.Create("a", { "className":"Link" }, this.Login.Inner.Top);
+				this.Login.Inner.Terms = Dom.Create("a", { "className":"Link" }, this.Login.Inner.Top);
+				this.Login.Inner.Beta = Dom.Create("div", { "className":"Message" }, this.Login.Inner.Top);
+				this.Login.Inner.User = Dom.Create("div", { "className":"Message" }, this.Login.Inner.Top);
 				
 				this.Login.Inner.UserInfo = {};
 				this.Login.Inner.UserInfo.Top = Dom.Create("div", { "className":"UserInfo", "style":"opacity:0; visibility:hidden;" }, this.Login.Inner.Top);
@@ -42,32 +64,36 @@ r.define(["Api/util/lang",
 
 				this.Login.Footer = {};
 				this.Login.Footer.Top = Dom.Create("div", { "className":"Footer" }, this.container);
-				// this.Login.Footer.BtnTutorial = Dom.Create("button", { "className":"Tutorial" }, this.Login.Footer.Top)
 				this.Login.Footer.BtnLanguage = Dom.Create("button", { "className":"Language" }, this.Login.Footer.Top)
 				
+				this.Login.Inner.Message.innerHTML = Lang.Nls("Main_LoginMessage_1");
+				this.Login.Inner.Beta.innerHTML = Lang.Nls("Main_LoginMessage_4");
 				this.Login.Inner.Buttons.BtnGo.innerHTML = Lang.Nls("Main_BtnGo");
-				// this.Login.Footer.BtnTutorial.innerHTML = Lang.Nls("Main_ButtonTutorial");
 				this.Login.Footer.BtnLanguage.innerHTML = Lang.Nls("Main_ButtonLanguage");
 			
-				// this.Login.Inner.Beta.innerHTML = Lang.Nls("Main_LoginMessage_4");
+				this.Login.Inner.About.innerHTML = Lang.Nls("Main_LoginLink_About");
+				this.Login.Inner.Terms.innerHTML = Lang.Nls("Main_LoginLink_Terms");
+			
 				this.Login.Inner.Buttons.BtnLog.addEventListener("click", this.BtnLog_OnClick.bind(this), false);
 				this.Login.Inner.Buttons.BtnGo.addEventListener("click", this.BtnGo_OnClick.bind(this), false);
 				this.Login.Footer.BtnLanguage.addEventListener("click", this.BtnLanguage_OnClick.bind(this), false);
+				
+				this.Login.Inner.About.addEventListener("click", this.onLnkPopup_Click.bind(this, this.popups.About));
+				this.Login.Inner.Terms.addEventListener("click", this.onLnkPopup_Click.bind(this, this.popups.Terms));
 			},
 			
 			UpdateLoginMessage : function(isLogged) {
-				var text = Lang.Nls("Main_LoginMessage_1") + "<br><br>" + Lang.Nls("Main_LoginMessage_4") + "<br><br>" ;
 				
 				if (isLogged) {
 					this.Login.Inner.Buttons.BtnLog.innerHTML = Lang.Nls("Main_BtnLog_Out");	
-					this.Login.Inner.Message.innerHTML = text + Lang.Nls("Main_LoginMessage_3");
+					this.Login.Inner.User.innerHTML = Lang.Nls("Main_LoginMessage_3");
 					this.Login.Inner.UserInfo.Label.innerHTML = this.controller.model.Details.name;
 					
 					Animate.Fade(this.Login.Inner.UserInfo.Top, true);
 				}
 				else {
 					this.Login.Inner.Buttons.BtnLog.innerHTML =  Lang.Nls("Main_BtnLog_In") ;
-					this.Login.Inner.Message.innerHTML = text + Lang.Nls("Main_LoginMessage_2");
+					this.Login.Inner.User.innerHTML = Lang.Nls("Main_LoginMessage_2");
 					this.Login.Inner.UserInfo.Label.innerHTML = "";
 					
 					Animate.Fade(this.Login.Inner.UserInfo.Top, false);
@@ -100,6 +126,10 @@ r.define(["Api/util/lang",
 				this.UpdateLoginMessage(ev.model.IsLogged);
 				
 				this.Login.Inner.Buttons.BtnGo.disabled = !this.controller.IsLogged();
+			},
+			
+			onLnkPopup_Click : function(popup, ev) {
+				popup.FadeIn();
 			}
 		})
 		
